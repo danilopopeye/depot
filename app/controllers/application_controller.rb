ae31466d5e923823c02cbd91906961596ac9f4a3
Danilo Sousa <danilopopeye@gmail.com>
2010-06-28 11:20:03 -0300

    Sessions migration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,14 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+
+	session :session_key => '_depot_session_id'
+
   helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+
+	# See ActionController::RequestForgeryProtection for details
+	# Uncomment the :secret if you're not using the cookie session store
+	protect_from_forgery :secret => 'cfc3a6b86af4766376813112a3fd62dc'
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
