1c6b6ab21f1602c4ca26383c311131e58b615c72
Danilo Sousa <danilopopeye@gmail.com>
2010-06-28 10:53:37 -0300

    Started the /store actions e layout

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+	def self.find_products_for_sale
+		find(:all, :order => "title")
+	end
+
 	validates_presence_of :title, :description, :image_url
 	validates_uniqueness_of :title
 	validates_length_of :title, :minimum => 10, :message => "must have at least {{count}} characters"
@@ -7,7 +11,7 @@ class Product < ActiveRecord::Base
 		:with => %r{\.(gif|jpg|png)$}i,
 		:message => 'must be a URL for GIF, JPG or PNG image. (gif|jpg|png)'
 	validate :price_must_be_at_least_a_cent
-	
+
 protected
 	def price_must_be_at_least_a_cent
 		errors.add(:price,'shold be at least 0.01') if price.nil? || price < 0.01
