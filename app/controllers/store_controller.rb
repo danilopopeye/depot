ae208f592340ace3b2bab8e8434dd29db1620a32
Danilo Sousa <danilopopeye@gmail.com>
2010-06-30 18:44:41 -0300

    Added button and action for empty the cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -16,6 +16,12 @@ class StoreController < ApplicationController
 		end
 	end
 
+	def empty_cart
+		session[:cart] = nil
+		flash[:notice] = "Your cart is currently empty"
+		redirect_to :action => :index
+	end
+
 private
 
 	def find_cart
