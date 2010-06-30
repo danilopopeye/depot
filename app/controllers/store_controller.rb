d88f7d63e0c46a322ce047a823f0fdfb78e9bd9b
Danilo Sousa <danilopopeye@gmail.com>
2010-06-30 16:30:01 -0300

    Rescue from invalid product and added flash on layout

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,12 +1,19 @@
 class StoreController < ApplicationController
-  def index
-  	@products = Product.find_products_for_sale
-  end
+	def index
+		@products = Product.find_products_for_sale
+	end
 
 	def add_to_cart
-		@cart = find_cart
-		product = Product.find(params[:id])
-		@cart.add_product(product)
+		begin
+			product = Product.find(params[:id])
+		rescue ActiveRecord::RecordNotFound
+			logger.error("Attempt to access invalid product #{params[:id]}")
+			flash[:notice] = "Invalid product"
+			redirect_to :action => :index
+		else
+			@cart = find_cart
+			@cart.add_product(product)
+		end
 	end
 
 private
