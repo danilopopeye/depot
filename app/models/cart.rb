b767fc24c4c2831263c718470e022cf643b88959
Danilo Sousa <danilopopeye@gmail.com>
2010-07-06 21:55:16 -0300

    Added the cartItem and the quantity of itens in cart;

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -6,6 +6,11 @@ class Cart
 	end
 
 	def add_product(product)
-		@items << product
+		current_product = @items.find {|item| item.product == product }
+		if current_product
+			current_product.increment_quantity
+		else 
+			@items << CartItem.new(product)
+		end
 	end
 end
